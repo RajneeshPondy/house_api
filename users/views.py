b362d7e3dd67cbeff33a1253b98763782a367a5d
RajneeshPython <rajneeshkmr668@gmail.com>
2021-01-29 11:58:57 +0530

    Added Serializer, ViewSet and api router/urls  for User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import viewsets
+
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
