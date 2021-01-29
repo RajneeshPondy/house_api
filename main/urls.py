69b7caa8bb70250d67ff48a3e625a800b100c558
RajneeshPython <rajneeshkmr668@gmail.com>
2021-01-29 13:58:41 +0530

    Added BasicAuthentication and SessionAuthentication for rest_framework

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,20 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from users import router as users_api_router
 
+auth_api_url = []
+
+if settings.DEBUG:
+    auth_api_url.append(path("verify/", include("rest_framework.urls")))
+
 api_url_patterns = [
-    path('accounts/', include(users_api_router.router.urls))
-] 
+    path("accounts/", include(users_api_router.router.urls)),
+    path("auth/", include(auth_api_url)),
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(api_url_patterns)),
+    path("admin/", admin.site.urls),
+    path("api/", include(api_url_patterns)),
 ]
