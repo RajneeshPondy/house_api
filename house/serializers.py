c52f43a0dbb024be8bdb729945e3c4dab2206bb1
RajneeshPython <rajneeshkmr668@gmail.com>
2021-01-30 12:50:37 +0530

    Implemented Houser Members and manager in serializer

--- house/serializers.py
+++ house/serializers.py
@@ -5,7 +5,12 @@ from house.models import House
 
 class HouseSerializer(serializers.ModelSerializer):
     members_count = serializers.IntegerField(read_only=True)
-    # members = serializers.IntegerField(read_only=True)
+    members = serializers.HyperlinkedRelatedField(
+        read_only=True, many=True, view_name="profile-detail"
+    )
+    manager = serializers.HyperlinkedRelatedField(
+        read_only=True, many=False, view_name="profile-detail"
+    )
 
     class Meta:
         model = House
@@ -18,9 +23,13 @@ class HouseSerializer(serializers.ModelSerializer):
             "manager",
             "description",
             "members_count",
-            # "members",
+            "members",
             "points",
             "completed_task_count",
             "not_completed_task_count",
         ]
-        read_only_fields = ["points", "completed_task_count", "not_completed_task_count"]
+        read_only_fields = [
+            "points",
+            "completed_task_count",
+            "not_completed_task_count",
+        ]
